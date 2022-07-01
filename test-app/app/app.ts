2d4c4eee5a2d81fef25aba27f4eae327ac879ce8
Edward Faulkner <edward@eaf4.com>
2022-07-01 16:16:51 -0400

    commenting out for now

--- test-app/app/app.ts
+++ test-app/app/app.ts
@@ -3,7 +3,7 @@ import Resolver from 'ember-resolver';
 import loadInitializers from 'ember-load-initializers';
 import config from 'test-app/config/environment';
 
-import { thing } from 'xrouter';
+// import { thing } from 'xrouter';
 
 export default class App extends Application {
   modulePrefix = config.modulePrefix;
